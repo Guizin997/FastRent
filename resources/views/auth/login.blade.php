0a9036e48a08f692bb950bafaaab3b9ac236df24
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-03-27 16:57:26 -0300

    Desenvolvimento do Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,20 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
-<form action="" class="justify-self-center">
+<form action="" class="justify-self-center flex">
     <div class="border-2 justify-self-center h-120 w-100 bg-indigo-800">
 
     </div>
 
     <div class="border-2 h-120 w-100">
 
-        <div class="border rounded-full mx-center mt-10 w-30 h-30"></div>
+        <div class="border rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
 
-        <input type="email" placeholder="E-mail" class="justify-self-center border rounded-xl w-60 h-10 pl-2 mt-10">
+        <input type="email" placeholder="E-mail" class="justify-self-center flex border rounded-xl w-60 h-10 pl-2 mt-8">
 
-        <input type="password" placeholder="Senha" class="justify-self-center border rounded-xl w-60 h-10 pl-2 mt-8">
+        <input type="password" placeholder="Senha" class="justify-self-center flex border rounded-xl w-60 h-10 pl-2 mt-8">
 
-        <button class="justify-self-center border border-amber-100 bg-indigo-800 text-slate-100 rounded-xl w-60 h-10 mt-8 cursor-pointer">Entrar</button>
+        <button class="justify-self-center justify-center flex border w-60 h-10 rounded-xl mt-6">Entrar</button>
 
-        <a href="#" class="flex underline text-sky-100 mt-2">Não possui uma conta?Cadastre-se</a>
+        <a href="#" class="justify-self-center flex underline text-cyan-500">Não possui uma conta?Cadastre-se</a>
     </div>
 </form>
