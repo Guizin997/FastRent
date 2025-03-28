de339406be1ef037412cda5e566670619dd57bba
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-03-28 13:05:08 -0300

    Design do Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,23 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
-<form action="" class="justify-self-center flex">
-    <div class="border-2 justify-self-center h-120 w-100 bg-indigo-800">
+<form action="" class="justify-self-center flex border-6 border-amber-200 my-40">
+
+    <div class="justify-self-center h-140 w-130 bg-blue-950">
 
     </div>
 
-    <div class="border-2 h-120 w-100">
+    <div class="h-140 w-130 bg-blue-950 border border-l-amber-200">
 
-        <div class="border rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
+        <div class="border-2 border-amber-200 rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
 
-        <input type="email" placeholder="E-mail" class="justify-self-center flex border rounded-xl w-60 h-10 pl-2 mt-8">
+        <h2 class="justify-self-center text-4xl font-extrabold text-amber-200">Login</h2>
 
-        <input type="password" placeholder="Senha" class="justify-self-center flex border rounded-xl w-60 h-10 pl-2 mt-8">
+        <input type="email" placeholder="E-mail" class="justify-self-center flex border border-amber-200 rounded-xl w-60 h-10 pl-2 mt-8 text-slate-50">
 
-        <button class="justify-self-center justify-center flex border w-60 h-10 rounded-xl mt-6">Entrar</button>
+        <input type="password" placeholder="Senha" class="justify-self-center flex border border-amber-200 rounded-xl w-60 h-10 pl-2 mt-8 text-slate-50">
 
-        <a href="#" class="justify-self-center flex underline text-cyan-500">Não possui uma conta?Cadastre-se</a>
+        <button class="justify-self-center justify-center flex border border-amber-200 w-60 h-10 rounded-xl mt-6 pt-2 cursor-pointer text-slate-50">Entrar</button>
+
+        <a href="#" class="justify-self-center flex underline text-cyan-500 text-slate-50 mt-2">Não possui uma conta?Cadastre-se</a>
     </div>
 </form>
