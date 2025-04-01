94978abd0b27f1f783fe178ed1e98190d9ac4be8
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-01 13:09:45 -0300

    Redesenhando o Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,20 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
-<form action="" class="justify-self-center flex border-6 rounded-xl border-amber-200 my-40">
+<form action="" class="justify-self-center flex rounded-xl my-40">
 
 
-    <div class="h-140 w-130 bg-blue-950 border border-l-amber-200">
+    <div class="h-140 w-130 bg-linear-to-t/shorter from-red-500 to-rose-950 rounded-xl shadow-2xl shadow-red-400">
 
-        <div class="border-2 border-amber-200 rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
+        <div class="border-3 border-slate-50 rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
 
-        <h2 class="justify-self-center text-4xl font-extrabold text-amber-200" style="font-family: Nunito">Login</h2>
+        <h2 class="justify-self-center text-4xl font-extrabold text-slate-50" style="font-family: Nunito">FastRent</h2>
 
-        <input type="email" placeholder="E-mail" style="font-family: Nunito" class="justify-self-center flex border border-amber-200 rounded-xl w-60 h-10 pl-2 mt-8 text-slate-50">
+        <input type="email" placeholder="E-mail" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950">
 
-        <input type="password" placeholder="Senha" style="font-family: Nunito" class="justify-self-center flex border border-amber-200 rounded-xl w-60 h-10 pl-2 mt-8 text-slate-50">
+        <input type="password" placeholder="Senha" style="font-family: Nunito" class="justify-self-center flex  rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950">
 
-        <button class="justify-self-center justify-center flex border border-amber-200 w-60 h-10 rounded-xl mt-6 pt-2 cursor-pointer text-slate-50" style="font-family: Nunito">Entrar</button>
+        <button class="justify-self-center justify-center flex ring-2 ring-amber-300 w-40 h-10 rounded-xl mt-10 pt-2 cursor-pointer text-slate-50" style="font-family: Nunito">Entrar</button>
 
         <a href="#" class="justify-self-center flex underline text-cyan-500 text-slate-50 mt-2" style="font-family: Nunito">Não possui uma conta?Cadastre-se</a>
     </div>
