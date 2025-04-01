795bee76d5cd6213e52ac9aada2849c91d8cac21
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-01 14:11:38 -0300

    Design oficial do Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,20 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
-<form action="" class="justify-self-center flex rounded-xl my-40">
+<form action="" class="justify-self-center flex my-40">
 
 
-    <div class="h-140 w-130 bg-linear-to-t/shorter from-red-500 to-rose-950 rounded-xl shadow-2xl shadow-red-400">
+    <div class="h-140 w-130 bg-linear-to-t/shorter from-red-500 to-rose-950 shadow-2xl shadow-red-400">
 
-        <div class="border-3 border-slate-50 rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
+        <div class="ring-3 ring-amber-300 shadow-xl shadow-amber-300/60 rounded-full mx-center mt-10 w-30 h-30 justify-self-center"></div>
 
-        <h2 class="justify-self-center text-4xl font-extrabold text-slate-50" style="font-family: Nunito">FastRent</h2>
+        <h2 class="justify-self-center text-4xl font-extrabold text-slate-50 mt-4" style="font-family: Nunito">FastRent</h2>
 
         <input type="email" placeholder="E-mail" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950">
 
-        <input type="password" placeholder="Senha" style="font-family: Nunito" class="justify-self-center flex  rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950">
+        <input type="password" placeholder="Senha" style="font-family:Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950">
 
-        <button class="justify-self-center justify-center flex ring-2 ring-amber-300 w-40 h-10 rounded-xl mt-10 pt-2 cursor-pointer text-slate-50" style="font-family: Nunito">Entrar</button>
+        <button class="justify-self-center justify-center flex bg-linear-to-t/shorter from-rose-950 to-red-500 w-40 h-10 rounded-xl mt-10 pt-2 cursor-pointer text-slate-50 shadow-xl shadow-red-900" style="font-family: Nunito" href="#">Entrar</button>
 
         <a href="#" class="justify-self-center flex underline text-cyan-500 text-slate-50 mt-2" style="font-family: Nunito">Não possui uma conta?Cadastre-se</a>
     </div>
