40b2eea18db76713f3a98a5bbabeb5c612eb7e3e
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-15 10:18:36 -0300

    Substituto do slide temporário

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,33 +25,10 @@
 
     </nav>
 
-    <div style="position: relative; scroll-snap-type: x mandatory; scroll-behavior: smooth; overflow: auto;" class="justify-self-center my-40 text-slate-700 text-center">
-      <ul class="flex gap-4">
-        <li class="border-2 w-230 h-120 rounded-xl bg-gray-300">1</li>
-      </ul>
-    </div>
-
-
-    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
-        </div>
-        <div class="carousel-item">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
-        </div>
-        <div class="carousel-item">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+<div class="w-200 h-170 bg-gray-400 text-slate-50">
+    <h2 ml-6 mt-4 text-4xl>Sobre nós</h2>
+    <p class="text-sm ml-6 mt-8">Porém ipsum dolor sit amet, consectetur adipiscing elit. Porém vitae justo nec quam fringilla fermentum. Integer euismod, porém vel dapibus posuere, metus nunc porta quam, non blandit sapien enim a nulla. Donec a justo, porém eu tincidunt risus. Porém curabitur id sapien ut nisi gravida lacinia.</p>
+</div>
+    
 </body>
 </html>
