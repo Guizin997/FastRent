454e3da067fdcb9842200a4924355dbbd4c462df
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-15 13:11:45 -0300

    navbar responsiva

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,37 +1,45 @@
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
 
+</head>
 <body class="bg-linear-to-br from-rose-950 to-red-500 bg-no-repeat bg-cover bg-fixed">
+    <nav class="h-30 bg-linear-to-b from-stone-950 to-rose-900/0 bg-cover">
+         
+        <div style="font-family: Nunito" class="text-slate-50 flex gap-8 pl-8">
+            <a class="my-12" href="">Carros</a>
+            <a class="my-12" href="">Marcas</a>
+            <a class="my-12" href="">Usuários</a>
+            <a class="my-12" href="">Ajuda</a>
+            <div class="h-15 w-15 border border-slate-50 rounded-full ml-102 my-7 sm:ml-70 md:ml-88 lg:ml-150 xl:ml-360"></div>
+        </div>
 
-<form method="POST" action="{{ route('register') }}" class="justify-self-center flex my-20">
-    @csrf
+    </nav>
 
-    <div class="h-170 w-130 bg-linear-to-t/shorter from-red-500 to-rose-950 shadow-2xl shadow-red-400">
-
-        <div style="background-image: ../img/logo.png" class="ring-3 ring-amber-300 shadow-xl shadow-amber-300/60 rounded-full mx-center mt-10 w-30 h-30 justify-self-center bg-no-repeat bg-cover bg-fixed"></div>
-
-        <h2 class="justify-self-center text-4xl font-extrabold text-slate-50 mt-4" style="font-family: Nunito">FastRent</h2>
-
-        <input type="text" id="name" name="name" placeholder="Nome" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autofocus autocomplete="name">
-        <x-input-error :messages="$errors->get('name')" class="mt-2" />
-
-        <input type="email" id="email" name="email" placeholder="E-mail" style="font-family:Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="username">
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-
-        <input type="password" id="passwored" name="password" placeholder="Senha" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="new-password">
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-
-        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Digite a senha novamente" style="font-family:Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="new-password">
-        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-
-        <x-primary-button class="ms-4">
-            {{ __('Cadastrar') }}
-        </x-primary-button>
-
-        <a href="{{url('/login')}}" class="justify-self-center flex underline text-cyan-500 text-slate-50 mt-6" style="font-family: Nunito">Já possui uma conta? Entrar</a>
-
+    <div class="flex gap-4 justify-self-center mt-22">
+    <div style="font-family: Nunito" class="card w-90 h-50 shadow-xl shadow-red-500 bg-gray-900 text-slate-50 rounded-xl border-2 border-red-200">
+        <div class="card-body">
+            <h5 class="card-title text-4xl mt-6 ml-6">Adicionar veículo</h6>
+            <p class="card-text text-sm text-gray-100 mt-8 ml-6">Adicionar carrocpara ser alugado ou comprado</p>
+            <button style="font-family: Nunito" class="h-8 bg-gray-800 border-green-400 border-2 text-slate-50 px-4 ml-6 mt-4 cursor-pointer rounded-xl">Adicionar</button>
+        </div>
     </div>
-</form>
+
+    <div style="font-family: Nunito" class="card w-90 h-50 shadow-xl shadow-red-500 bg-gray-900 text-slate-50 rounded-xl border-2 border-red-200">
+        <div class="card-body">
+            <h5 class="card-title text-4xl mt-6 ml-6">Adicionar marca</h6>
+            <p class="card-text text-sm text-gray-100 mt-8 ml-6">Adicionar nova marca parceira com nossa empresa</p>
+            <button style="font-family: Nunito" class="h-8 bg-gray-800 border-green-400 border-2 text-slate-50 px-4 ml-6 mt-4 cursor-pointer rounded-xl">Adicionar</button>
+        </div>
+    </div>
+    </div>
 </body>
+</html>
