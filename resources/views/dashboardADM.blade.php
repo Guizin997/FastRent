d734ffd410562db41d1cdc49640a08bcfe9e7ed7
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-14 22:03:49 -0300

    Cards no dashboard do adm

--- resources/views/dashboardADM.blade.php
+++ resources/views/dashboardADM.blade.php
@@ -16,42 +16,26 @@
          
         <div style="font-family: Nunito" class="text-slate-50 flex gap-8 pl-8">
             <a class="my-12" href="">Carros</a>
-            <a class="my-12" href="">Alugar</a>
-            <a class="my-12" href="">Por Marca</a>
-            <a class="my-12" href="">Usuário</a>
+            <a class="my-12" href="">Marcas</a>
+            <a class="my-12" href="">Usuários</a>
             <a class="my-12" href="">Ajuda</a>
             <div class="h-15 w-15 border border-slate-50 rounded-full ml-102 my-7"></div>
         </div>
 
     </nav>
 
-    <div style="position: relative; scroll-snap-type: x mandatory; scroll-behavior: smooth; overflow: auto;" class="justify-self-center my-40 text-slate-700 text-center">
-      <ul class="flex gap-4">
-        <li class="border-2 w-230 h-120 rounded-xl bg-gray-300">1</li>
-      </ul>
+    <div style="font-family: Nunito" class="card w-20 h-20 cursor-pointer shadow-xl shadow-red-200 bg-gray-800 text-slate-50">
+        <div class="card-body">
+            <h5 class="card-title">Adicionar veículo</h6>
+            <p class="card-text">Adicionar carro para ser algugado ou comprado</p>
+        </div>
     </div>
 
-
-    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>
+    <div style="font-family: Nunito" class="card w-20 h-20 cursor-pointer shadow-xl shadow-red-200 bg-gray-800 text-slate-50">
+        <div class="card-body">
+            <h5 class="card-title">Adicionar marca</h6>
+            <p class="card-text">Adicionar nova marca parceira com nossa empresa</p>
         </div>
-        <div class="carousel-item">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text></svg>
-        </div>
-        <div class="carousel-item">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text></svg>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
 </body>
 </html>
