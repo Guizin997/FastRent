52de82c8a887a80617f7a7ce7e3613cb64a952df
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2025-04-21 22:55:59 -0300

    Tela para adicionar categoria

---
+++ resources/views/AdicionarCategoria.blade.php
@@ -0,0 +1,38 @@
+<x-auth-session-status class="mb-4" :status="session('status')" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+<body class="bg-gray-900">
+    
+<form method="POST" action="{{ route('register') }}" class="justify-self-center flex my-33">
+    @csrf    
+
+
+    <div class="h-173 w-130 bg-linear-to-t/shorter from-red-500 to-rose-950 rounded-2xl shadow-xl shadow-red-700/60">
+
+        <div class="ring-3 ring-amber-300 shadow-xl shadow-amber-300/60 rounded-full mx-center mt-10 w-30 h-30 justify-self-center bg-no-repeat bg-cover bg-fixed"><img class="rounded-full" src="{{ asset('assets/images/fastRentLogo.png') }}" alt="FastRent Logo"></div>
+
+        <h2 class="justify-self-center text-4xl font-extrabold text-slate-50 mt-4" style="font-family: Nunito">Adiconar Categoria</h2>
+
+        <input type="text" id="name" name="name" placeholder="Nome da categoria *" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autofocus autocomplete="name">
+        <x-input-error :messages="$errors->get('name')" class="mt-2 ml-26 w-80" />
+
+        <input type="number" id="email" name="text placeholder="Quantidade de veículos *" style="font-family:Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="username">
+        <x-input-error :messages="$errors->get('email')" class="mt-2 ml-26 w-80" />
+
+        <input type="number" id="text" name="password" placeholder="Passageiros por carro*" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="new-password">
+        <x-input-error :messages="$errors->get('password')" class="mt-2 ml-26 w-80" />
+
+        <input type="number" id="password_confirmation" name="password_confirmation" placeholder="Descrição da categoria *" style="font-family:Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="new-password">
+        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 ml-26 w-80" />
+
+        <x-primary-button class="ms-3">
+                {{ __('Cadastrar') }}
+        </x-primary-button>
+        <a href="{{url('/login')}}" class="justify-self-center flex underline text-cyan-500 text-slate-50 mt-4" style="font-family: Nunito">Já possui uma conta?Entrar</a>
+    </div>
+</form>
+
+</body>
