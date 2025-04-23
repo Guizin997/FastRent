4f7ab77c71b92170679286fd9d787cbd53955db9
Guizin997 <guiguirussas@gmail.com>
2025-04-23 12:36:57 -0300

    espaçamento do erro do lgn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,10 +17,10 @@
         <h2 class="justify-self-center text-4xl font-extrabold text-slate-50 mt-4" style="font-family: Nunito">FastRent</h2>
 
         <input type="email" name="email" :value="old('email')" placeholder="E-mail *" style="font-family: Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autofocus autocomplete="username" />
-        <x-input-error :messages="$errors->get('email')" class="mt2" />
+        <x-input-error :messages="$errors->get('email')" class="ml-22 mt2" />
 
         <input type="password" name="password" placeholder="Senha *" style="font-family:Nunito" class="justify-self-center flex rounded-xl w-80 h-12 pl-2 mt-8 text-slate-50 bg-indigo-950" required autocomplete="current-password" />
-        <x-input-error :messages="$errors->get('password')" class="mt2" />
+        <x-input-error :messages="$errors->get('password')" class="ml-22 mt2" />
 
         <div class="block mt-4 px-26">
             <label for="remember_me" class="inline-flex items-center">
